b5bcb7252c36674fef60f79d4011830bb30ab546
Tilak Joshi <Tilak Joshi@LIBRAPRO>
2023-05-28 13:01:53 +0545

    Add project files.

---
+++ HRM/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Student_Table()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
